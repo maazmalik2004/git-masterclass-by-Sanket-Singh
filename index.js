8b3764c74f3911268957600ba55258fffc5f59c9
Maaz Malik <maazmalik2004@gmail.com>
2024-07-12 18:31:59 +0530

    tag added in the parent commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("hello world");
 console.log("we are going to see how the head woeks 5:09");
 //making some more changes before advancing the master pointer
+
+//making some more changes after tag
